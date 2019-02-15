d52a5b82d18c01d0aafc2da591540b6fd4ad95fc
Alex Casassovici <ac@azarus.io>
2019-02-15 14:47:18 -0800

    update eol thumb

--- script/get_more_EOL_pics.rb
+++ script/get_more_EOL_pics.rb
@@ -3,6 +3,9 @@ require 'active_support'
 require 'mysql2'
 require 'net/http'
 require 'uri'
+require 'open-uri'
+require 'nokogiri'
+
 #require 'iconv'
 
 #output_sname = File.new("database_sname.sql", "wb")
@@ -16,43 +19,20 @@ socket = "/var/run/mysqld/mysqld.sock"
 client = Mysql2::Client.new(:database => database, :username => username, :password => password, :socket => socket)
 
 ###gets the IDS that have photos
-ids_to_reprocess = client.query("SELECT id FROM eolsnames WHERE picture > 0")
+ids_to_reprocess = client.query("SELECT id FROM eolsnames")
 ids_to_reprocess = ids_to_reprocess.to_a.map{|t| t["id"]}
 
 
 
-ids_to_reprocess.each do |filenumber|
-  print "\rprocessing file #{filenumber}"
+ids_to_reprocess.each do |id|
+  print "\rprocessing species #{id}"
   #file = File.new("/var/EOL/EOL/#{filenumber}.json","r")
   begin
-    	line = Net::HTTP.get URI.parse("http://www.eol.org/api/pages/1.0/#{filenumber}.json?images=30&common_names=1&details=1") #file.gets
-    	if line.match(/^\</).nil? && line.match(/^\{/).nil? then
-    	  raise DBTechnicalError.new "file does not start with < or { and is probably empty - SKIPPING IT"
-  	  end
-  	  if line.match(/worms/i).nil? && line.match(/fishbase/i).nil? then
-  	    raise DBTechnicalError.new "no worms nor fishbase record, this is not in the sea - Skipping it"
-	    end
+    doc = Nokogiri::HTML(open("https://eol.org/pages/#{filenumber}"))
+    thumbnail_href = doc.css("a.hero-link img")[0]["src"]
 
-    	json = JSON.parse(line)
-
-    picture = 0
-    if !json["dataObjects"].nil?
-      json["dataObjects"].each do |dataset|
-        picture = picture+1
-        if dataset.to_s.match(/MediaURL/i)
-          break  ## we found one image
-        end
-      end
-    end
-
-    ##ADDING SNAME
-    if !json["identifier"].nil?
-      #result = client.query(query_head+"(#{json["identifier"]},'#{json["scientificName"].gsub(/\`/,"").gsub(/'/, "\\\\'")}','#{ActiveSupport::JSON.encode(json["taxonConcepts"])}','#{ActiveSupport::JSON.encode(json["dataObjects"])}','#{picture.to_s}','2011-08-01 00:03:55','2011-08-01 00:03:55');")#.dump ##dump prevents unescaping
-      data = client.escape((json["dataObjects"]).to_json )
-      sql = "UPDATE eolsnames SET eolsnames.data = '#{data}', eolsnames.picture = '#{picture.to_s}' WHERE eolsnames.id = #{filenumber}"
-      result = client.query(sql)
-      puts "\rdata added for #{filenumber}"
-
+    sql = "UPDATE eolsnames SET eolsnames.picture = '1', eolsname.thumbnail_href = '#{thumbnail_href}' WHERE eolsnames.id = #{id}"
+    
     else
       raise DBTechnicalError.new "No usable data"
     end
---
+++ script/refresh_eol_thumb.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/ruby1.9.1
+require 'active_support'
+require 'mysql2'
+require 'net/http'
+require 'uri'
+#require 'iconv'
+require 'open-uri'
+require 'nokogiri'
+
+#output_sname = File.new("database_sname.sql", "wb")
+#output_cname = File.new("database_cname.sql", "wb")
+
+
+database = "diveboard"
+username = "dbuser"
+password = ENV["PROD_DB"]
+socket = "/var/run/mysqld/mysqld.sock"
+client = Mysql2::Client.new(:database => database, :username => username, :password => password, :socket => socket)
+
+###gets the IDS that have photos
+ids_to_reprocess = client.query("SELECT id FROM eolsnames")
+ids_to_reprocess = ids_to_reprocess.to_a.map{|t| t["id"]}
+
+
+
+ids_to_reprocess.each do |id|
+  sql = ""
+  print "\rprocessing id #{id}"
+  begin
+    doc = Nokogiri::HTML(open("https://eol.org/pages/#{id}"))
+    begin
+      thumbnail_href = doc.css("a.hero-link img")[0]["src"]
+      throw if thumbnail_href.nil?
+      sql = "UPDATE eolsnames SET eolsnames.picture = '1', eolsname.thumbnail_href = '#{thumbnail_href}' WHERE eolsnames.id = #{id}"
+    rescue
+      sql = "UPDATE eolsnames SET eolsnames.picture = '0', eolsname.thumbnail_href = NULL WHERE eolsnames.id = #{id}"
+    end
+    result = client.query(sql)
+
+  rescue
+  	puts "\n>>>>>>>>>>>>>>>incorrect data #{id}<<<<<<<<<<<<<<<<"
+  	print sql
+  	puts $!
+  end
+
+end
+
